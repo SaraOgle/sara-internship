0881d4850399a62dac950924133867d32b6d0fa8
SaraOgle <259494087+SaraOgle@users.noreply.github.com>
2026-05-09 14:51:24 -0700

    ExploreItems

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import axios from "axios";
 import 'keen-slider/keen-slider.min.css'
 import { useKeenSlider } from 'keen-slider/react'
+import Items from "../Items";
 
 const NewItems = () => {
-  const [users, setUsers] = useState([]);
+  const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true); 
 
   const [sliderRef, instanceRef] = useKeenSlider({
@@ -16,20 +16,23 @@ const NewItems = () => {
       spacing: 15,
     },
     breakpoints: {
-      "(max-width: 1000px)": {
+      "(max-width: 1200px)": {
+        slides: { perView: 3, spacing: 10 },
+      },
+      "(max-width: 760px)": {
         slides: { perView: 2, spacing: 10 },
       },
-      "(max-width: 500px)": {
+      "(max-width: 560px)": {
         slides: { perView: 1, spacing: 10 },
       },
     },
   });
 
-  async function getUsers() {
+  async function getItems() {
     setLoading(true);
     try {
       const { data } = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems");
-      setUsers(data);
+      setItems(data);
     } catch (error) {
       console.error("Error fetching collections:", error);
     } finally {
@@ -38,43 +41,15 @@ const NewItems = () => {
   }
 
   useEffect(() => {
-    getUsers();
+    getItems();
   }, []);
 
   useEffect(() => {
     if (instanceRef.current) {
       instanceRef.current.update();
     }
-  }, [users, loading, instanceRef]);
-    
-  const Countdown = ({ expiryDate }) => {
-  const [timeLeft, setTimeLeft] = useState("");
-
-  useEffect(() => {
-    const calculateTime = () => {
-      const time = expiryDate - Date.now();
-      
-      if (time <= 0) {
-        setTimeLeft("Expired");
-        return;
-      }
-
-      const seconds = Math.floor((time / 1000) % 60);
-      const minutes = Math.floor((time / 1000 / 60) % 60);
-      const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
-
-      setTimeLeft(`${hours}h ${minutes}m ${seconds}s`);
-    };
-
-    calculateTime();
-
-    const interval = setInterval(calculateTime, 1000);
-
-    return () => clearInterval(interval);
-  }, [expiryDate]);
-
-  return <>{timeLeft}</>;
-};
+  }, [items, loading, instanceRef]);
+  
 
   return (
     <section id="section-items" className="no-bottom">
@@ -88,9 +63,10 @@ const NewItems = () => {
           </div>
           <div className="navigation-wrapper" style={{ position: 'relative' }}>
             <div ref={sliderRef} className="keen-slider">
-          {loading
+          
+        {loading
                 ?
-                  new Array(4).fill(0).map((_, index) => (
+                  new Array(4).fill(0).map((items, index) => (
                     <div className="keen-slider__slide" key={index}>
                       <div className="nft__item">
                         <div className="author_list_pp">
@@ -105,68 +81,17 @@ const NewItems = () => {
                         </div>
                       </div>
                     </div>
-                  ))
-                :
-                users.map((user) => (
-            <div className="keen-slider__slide" key={user.id}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                  >
-                    <img className="lazy" src={user.authorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="de_countdown">
-                  {user.expiryDate ? (
-                    <Countdown expiryDate={user.expiryDate} />
-                  ) : (
-                    "EXPIRED"
-                    )}
-                    </div>
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                  <Link to="/item-details">
-                    <img
-                      src={user.nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
-                </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>{user.title}</h4>
-                  </Link>
-                  <div className="nft__item_price">{user.price} ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>{user.likes}</span>
-                  </div>
-                </div>
-              </div>
+                  )
+                ) : (
+          items.map((item) => (
+            <div className="keen-slider__slide" key={item.id}>
+            <Items item={item} />
             </div>
-          ))}
+            
+          ))
+        )}
+      
+                  
           </div>
           <button 
             className="arrow arrow--left" 
